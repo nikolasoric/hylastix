d17c25c660c06fc38753759b9de63f33edbfa3a3
Nikola Soric <72814546+nikolasoric@users.noreply.github.com>
2025-04-23 12:49:37 +0200

    Get Admin User

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
+output "admin_username" {
+  value       = var.admin_username
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -68,5 +68,5 @@ variable "vm_size" {
 variable "admin_username" {
   description = "Admin username for the VM"
   type        = string
-  default     = "azureuser"
+  default     = "ubuntu"
 }
